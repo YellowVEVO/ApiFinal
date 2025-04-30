a53c083ac209249755cbc97231da3aea9cc09128
YellowVEVO <ahmadm708@gmail.com>
2025-04-29 23:40:02 -0600

    fix on user route

--- library-access-system/server/seedUsers.js
+++ library-access-system/server/seedUsers.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require('./models/user');
+const User = require('./models/User');
 require('dotenv').config({ path: './server/.env' });
 
 const users = [
